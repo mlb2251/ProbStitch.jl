ede744e82751a7bdde29096bd30b17895518237c
Kavi Gupta <kavi@berkeley.edu>
2025-02-27 14:12:20 -0500

    add tests

---
+++ tests/unit/size.jl
@@ -0,0 +1,19 @@
+
+# julia test suite for size
+
+using Test
+
+using ProbStitch: Corpus, size
+
+@testset "make sure corpus is sized correctly" begin
+    @test size(Corpus(["a", "b"])) == 2
+    @test size(Corpus(["a", "b", "c"])) == 3
+    @test size(Corpus(["a", "b", "c", "d"])) == 4
+    @test size(Corpus(String[])) == 0
+end
+
+@testset "test size of s expressions" begin
+    @test size(Corpus(["(a)"])) == 1
+    @test size(Corpus(["(a b c (d))"])) == 4
+    # @test size(Corpus(["((a b))"])) == 2
+end
